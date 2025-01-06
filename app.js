df132a0f9a8feafc963a4afafaf0f12c5e864479
Aamir-pathan-ekcs <maakhan@ekcs.co>
2025-01-06 23:28:27 +0530

    voice deatils saved data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
@@ -15,6 +16,10 @@ mongose.connect('mongodb+srv://maakhan:iqMZFeqCyvDlNh72@cluster0.l9s6zof.mongodb
 });
 const voiceData = require('./models/dataModel');
 
+app.get('/dashboard', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'chart', 'index.html'));
+})
+
 app.post('/voice', function(req, res){
     const dataForm = req.body;
     console.log(dataForm);
@@ -23,6 +28,31 @@ app.post('/voice', function(req, res){
     .then(()=>{
         res.status(201).json({Message: 'data saved successfylly'})
     }).catch((err)=>res.status(500).json({error : err.message}));
+})
+
+app.get('/promoCodeCount', async(req, res)=>{
+    try{
+        const itemFound = async(item)=>{
+            return await voiceData.countDocuments(item); 
+        }
+        const promoCodeFive = { promo: 'DISCOUNT5'};
+        const promoCodeTen = { promo: 'DISCOUNT10'};
+        const promoCodeTwenty = { promo: 'DISCOUNT20'};
+        const promoCodeThirty = { promo: 'DISCOUNT30'};
+
+        const fiveCount = await itemFound(promoCodeFive);
+        const tenCount = await itemFound(promoCodeTen);
+        const twentyCount = await itemFound(promoCodeTwenty);
+        const thirtyCount = await itemFound(promoCodeThirty);
+        res.json({
+            five: fiveCount,
+            ten: tenCount,
+            twenty: twentyCount,
+            thirty: thirtyCount
+        })
+    }catch(err) {
+        console.error('counting error', err);
+    }
 })
 
 app.listen(PORT, ()=>{
